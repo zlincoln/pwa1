0353b561893e97e06f205d56eda85af4c1377204
Zach <zach@bluehousegroup.com>
2014-01-11 16:35:04 -0500

    added avg function

--- wk1/goal2_assign_JSpractice/js/main.js
+++ wk1/goal2_assign_JSpractice/js/main.js
@@ -1,92 +1,54 @@
 // self-executing function
 (function(){
 
-    /*******************************************
+		console.log('------ Goal2: Assignment: JavaScript Practice ----------');
 
-     STUDENTS ASSIGNMENT
+		console.log("1. avg of an array of numbers");
+		var avgNumbers = function(arr){
+			var sum = 0;
+			for(var i = 0; i < arr.length; i++){
+				sum += arr[i];
+			}
+			return sum/arr.length;
+		};
 
-     1.  create a function named 'avgNumbers'
-         - accept 1 parameter into the function that will be an array of unlimited numbers
-         - find the average of all the numbers
-         - return the average from the function
-         - console.log the answer outside of the function
+		console.log('avg number = ', avgNumbers([1,2,3,4,5]));
 
-     2.  create a function named 'fullName'
-         - accept 2 parameters into the function that are strings (firstname and lastname)
-         - return the name after it has been concatenated
-         - console.log the answer outside of the function
+		//--------------------------------------------------------
+		console.log("2. concat first and last name");
 
-     3.  create a function named 'wordCount'
-         - accept 1 parameter into the function that is a long string of text words
-         - create a function that counts all the words and return the answer
-         - console.log the answer outside of the function
+				//PUT FUNCTION HERE
 
-     4.  create a function named 'charCount'
-         - accept 1 parameter into the function that is a long string of text
-         - return length of the array of string characters
-         - console.log the answer outside of the function
+		console.log(fullName('James', 'Bond'));
 
-     5.  create a function named 'vowelsInWord'
-         - accept 1 parameter into the function that is a a one word string
-         - return the number of vowels in the word
-         - console.log the answer outside of the function
+		//--------------------------------------------------------
+		console.log("3. word count");
+		var ipsum = "this is test text that is being used as input to a function"
 
-     6.  create a function named 'findNum'
-         - accepts 2 parameters into the function - 1. array of numbers, 2. boolean
-         - if the second parameter being passed is "false" or null then
-             - create an array with all of the odd numbers from
-                the array
-         - else - create an array with all of the even numbers
-            from the array
-         - return the array
-         - console.log the answer outside of the function
-     ********************************************/
+				//PUT FUNCTION HERE
 
-    console.log('------ Goal2: Assignment: JavaScript Practice ----------');
+		console.log(wordCount(ipsum));
 
-    console.log("1. avg of an array of numbers");
-    var avgNumbers = function(arr){
+		//--------------------------------------------------------
+		console.log("4. sentence char count");
 
-        //CODE GOES HERE
-    };
+				//PUT FUNCTION HERE
 
-    console.log('avg number = ', avgNumbers([1,2,3,4,5]));
+		console.log(charCount(ipsum));
 
-    //--------------------------------------------------------
-    console.log("2. concat first and last name");
+		//--------------------------------------------------------
+		console.log("5. how many vowels in a word");
 
-        //PUT FUNCTION HERE
+				//PUT FUNCTION HERE
 
-    console.log(fullName('James', 'Bond'));
+		console.log(vowelsInWord('JavaScript'));
 
-    //--------------------------------------------------------
-    console.log("3. word count");
-    var ipsum = "this is test text that is being used as input to a function"
+		//--------------------------------------------------------
+		console.log("6. find number and create an array of even or odd numbers");
 
-        //PUT FUNCTION HERE
+				//PUT FUNCTION HERE
 
-    console.log(wordCount(ipsum));
-
-    //--------------------------------------------------------
-    console.log("4. sentence char count");
-
-        //PUT FUNCTION HERE
-
-    console.log(charCount(ipsum));
-
-    //--------------------------------------------------------
-    console.log("5. how many vowels in a word");
-
-        //PUT FUNCTION HERE
-
-    console.log(vowelsInWord('JavaScript'));
-
-    //--------------------------------------------------------
-    console.log("6. find number and create an array of even or odd numbers");
-
-        //PUT FUNCTION HERE
-
-    console.log(findNum([31,22,4,67,83,6,5,4]));
-    console.log(findNum([31,22,4,67,83,6,5,4], false));
+		console.log(findNum([31,22,4,67,83,6,5,4]));
+		console.log(findNum([31,22,4,67,83,6,5,4], false));
 
 })();
