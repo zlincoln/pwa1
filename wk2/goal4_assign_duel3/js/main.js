e67775f98e0b3d9cdfbb0454e9db1349655cc198
Zach <zach@bluehousegroup.com>
2014-01-18 15:49:26 -0500

    started work on updating interface

--- wk2/goal4_assign_duel3/js/main.js
+++ wk2/goal4_assign_duel3/js/main.js
@@ -1,56 +1,79 @@
 /*
 - Zachary Lincoln
-- Duel 1
-- Sat 11, 2014
+- Duel 3
+- Sat 18, 2014
 */
+document.addEventListener('DOMContentLoaded', function(){
+	var player1 = {
+		name: 'Kabal',
+		health: 100,
+		damage: 50,
+		DOMContainer: document.querySelectorAll("#kabal p")[0]
+	}
 
-var player1 = ['Kabal', 100, 50];
+	var player2 = {
+		name: 'Kratos',
+		health: 100,
+		damage: 50,
+		DOMContainer: document.querySelectorAll('#kratos p')[0]
+	}
 
-var player2 = ['Kratos', 100, 50];
+	var playerArray = [player1,player2];
 
-function fight(){
-	var rounds = 10,
-			outcome = false,
-			current_round = 0;
+	function updateInterface(){
+		var outcome = false;
+		for(var i = 0; i < playerArray.length; i++){
+			playerArray[i].DOMContainer.innerHTML = '<h4>'+playerArray[i].name+'</h4>'+'<span>'+playerArray[i].health+'</span>';
+		}
+		// outcome = winnerCheck(rounds, current_round);
+	}
 
-	alert(player1[0]+": "+player1[1]+" ** START ** "+player2[0]+": "+player2[1]);
-	for(var i = 0; i < rounds; i++){
-		current_round = i+1;
-		player1[1] -= Math.floor(Math.random() * (player2[2] - (player2[2]/2)) + (player2[2]/2));
-		player2[1] -= Math.floor(Math.random() * (player1[2] - (player1[2]/2)) + (player1[2]/2));
-		outcome = winnerCheck(rounds, current_round);
-		if(outcome.game_over){
-			break;
+	function fight(){
+		var rounds = 10,
+				outcome = false,
+				current_round = 0;
+
+		alert(player1[0]+": "+player1[1]+" ** START ** "+player2[0]+": "+player2[1]);
+		for(var i = 0; i < rounds; i++){
+			current_round = i+1;
+			player1[1] -= Math.floor(Math.random() * (player2[2] - (player2[2]/2)) + (player2[2]/2));
+			player2[1] -= Math.floor(Math.random() * (player1[2] - (player1[2]/2)) + (player1[2]/2));
+			outcome = winnerCheck(rounds, current_round);
+			if(outcome.game_over){
+				break;
+			}else{
+				alert(player1[0]+": "+player1[1]+" ** ROUND "+current_round+" ** "+player2[0]+": "+player2[1]);
+			}
+		}
+		if(!outcome.winner){
+			alert('Players Tied!');
 		}else{
-			alert(player1[0]+": "+player1[1]+" ** ROUND "+current_round+" ** "+player2[0]+": "+player2[1]);
+			alert(outcome.winner+" Wins!!");
 		}
 	}
-	if(!outcome.winner){
-		alert('Players Tied!');
-	}else{
-		alert(outcome.winner+" Wins!!");
-	}
-}
 
-function winnerCheck(rounds, current_round){
-	var outcome = {
-		game_over: false,
-		winner: false
-	}
-	if(rounds == current_round){
-		outcome.game_over = true;
-		if(player1[1] != player2[1]){
-			outcome.winner = (player1[1] > player2[1]) ? player1[0] : player2[0];
+	function winnerCheck(rounds, current_round){
+		var outcome = {
+			game_over: false,
+			winner: false
 		}
-	}else{
-		if(player1[1] <= 0 && player2[1] <=0){
+		if(rounds == current_round){
 			outcome.game_over = true;
-		}else if(player1[1] <= 0 || player2[1] <= 0){
-			outcome.game_over = true;
-			outcome.winner = (player1[1] > player2[1]) ? player1[0] : player2[0];
+			if(player1[1] != player2[1]){
+				outcome.winner = (player1[1] > player2[1]) ? player1[0] : player2[0];
+			}
+		}else{
+			if(player1[1] <= 0 && player2[1] <=0){
+				outcome.game_over = true;
+			}else if(player1[1] <= 0 || player2[1] <= 0){
+				outcome.game_over = true;
+				outcome.winner = (player1[1] > player2[1]) ? player1[0] : player2[0];
+			}
 		}
+		return outcome;
 	}
-	return outcome;
-}
 
-fight();
+	updateInterface();
+
+	// fight();
+});
