08c9257de005443e8d07faab806433720dad5ac2
Zach <zach@bluehousegroup.com>
2014-01-18 17:08:32 -0500

    got event listeners working

--- wk2/goal4_assign_guessing_game/js/main.js
+++ wk2/goal4_assign_guessing_game/js/main.js
@@ -8,7 +8,24 @@
  */
 
 //Game variables
-(function ()
+document.addEventListener('DOMContentLoaded', function(){
 
+	var randNum = Math.floor((Math.random() * 10) + 1),
+			el = {
+				input: document.getElementById('input'),
+				button: document.getElementsByTagName('button')[0],
+				outputContainer: document.getElementById('output')
+			},
+			gameOver = false;
 
-})();
+	function evalGuess(e){
+		keycode = (e.keyCode) ? e.keyCode : null;
+		if(keycode == 13 || keycode == null){
+			console.log('heyo');
+		}
+	}
+
+	document.querySelector('button').addEventListener('click', evalGuess);
+	document.querySelector('input').addEventListener('keydown', evalGuess);
+
+});
